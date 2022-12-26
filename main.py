5e08bc6e7eda81045b6cf91e3fbb9cec196d5247
CaptainDeathead <115883954+CaptainDeathead@users.noreply.github.com>
2022-12-26 13:17:10 +0800

    Added basic code for movement

---
+++ main.py
@@ -0,0 +1,44 @@
+import GPIO as pins
+import pygame
+
+# set up pins 2 and 3 as outputs
+pins.setup(2, pins.OUT)
+pins.setup(3, pins.OUT)
+
+pygame.init()
+
+run = True
+
+def forward():
+    pins.output(2, pins.HIGH)
+    pins.output(3, pins.HIGH)
+
+def backward():
+    pins.output(2, pins.LOW)
+    pins.output(3, pins.LOW)
+
+def left():
+    pins.output(2, pins.HIGH)
+    pins.output(3, pins.LOW)
+
+def right():
+    pins.output(2, pins.LOW)
+    pins.output(3, pins.HIGH)
+
+while run == True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_w]:
+        forward()
+    elif keys[pygame.K_s]:
+        backward()
+    elif keys[pygame.K_a]:
+        left()
+    elif keys[pygame.K_d]:
+        right()
+
+    pygame.time.Clock().tick(60)
